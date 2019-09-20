3c5e7fc4cbb4cfe1752321ffaf240cb0d70829e2
Dan Smith <daniel.smith8795@gmail.com>
2019-09-19 21:29:39 -0500

    Fixed showdays function

--- models/timer.js
+++ models/timer.js
@@ -2,12 +2,13 @@ var moment = require("moment");
 moment().format();
 //$("#checkout").trim().val()
 //$("#dueDate").trim().val()
+var daysLeft = "MM-DD-YYYY";
 var showDaysLeft = function() {
   var checkoutDate = moment("09-19-2019", "MM-DD-YYYY");
   //in real app pull these dates from the database
   var dueDate = moment("09-20-2019", "MM-DD-YYYY");
   //in real app pull these dates from the database
-  var daysLeft = moment(checkoutDate).diff(dueDate, "days");
+  daysLeft = moment(dueDate).diff(checkoutDate, "days");
   console.log(daysLeft);
 };
 
