8c5c941739a72d67aa45fbde9a7c70e4e3f67ebd
Dan Smith <daniel.smith8795@gmail.com>
2019-09-20 13:38:17 -0500

    Transitioning code to use db info

--- models/timer.js
+++ models/timer.js
@@ -1,15 +1,18 @@
 var moment = require("moment");
 moment().format();
+var db = require("./index");
 //$("#checkout").trim().val()
 //$("#dueDate").trim().val()
 var daysLeft = "MM-DD-YYYY";
 var showDaysLeft = function() {
-  var checkoutDate = moment("09-19-2019", "MM-DD-YYYY");
-  //in real app pull these dates from the database
-  var dueDate = moment("09-20-2019", "MM-DD-YYYY");
-  //in real app pull these dates from the database
-  daysLeft = moment(dueDate).diff(checkoutDate, "days");
-  console.log(daysLeft);
+  for (var i = 0; i < db.length; i++) {
+    var checkoutDate = moment(db.i.date_borrowed, "MM-DD-YYYY");
+    //in real app pull these dates from the database
+    var dueDate = moment(db.i.due_date, "MM-DD-YYYY");
+    //in real app pull these dates from the database
+    daysLeft = moment(dueDate).diff(checkoutDate, "days");
+    console.log(daysLeft);
+  }
 };
 
 showDaysLeft();
