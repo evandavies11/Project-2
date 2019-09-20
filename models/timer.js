783dd384e74ab08b4181443d9dcaafc9cae095ab
Dan Smith <daniel.smith8795@gmail.com>
2019-09-20 14:01:58 -0500

    Commented out unused variable

--- models/timer.js
+++ models/timer.js
@@ -1,6 +1,6 @@
 var moment = require("moment");
 moment().format();
-var fs = require("fs");
+//var fs = require("fs");
 /* var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
