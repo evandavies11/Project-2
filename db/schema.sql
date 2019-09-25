940ab1eb46c7ed74009b8af870abf626c713bee8
evandavies11 <evandavies11@gmail.com>
2019-09-24 20:50:00 -0500

    fixed the stylesheet

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,15 @@ CREATE DATABASE books_db;
 
 USE books_db;
 
-CREATE TABLE books (
+CREATE TABLE books
+(
 
-id INT(100) NOT NULL,
-title VARCHAR(100) NOT NULL,
-name_of_borrower VARCHAR(100) NOT NULL,
-date_borrowed 	DATE NOT NULL,
-due_date DATE NOT NULL,
-PRIMARY KEY (id)
- );
+    id INT(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    name_of_borrower VARCHAR(100) NOT NULL,
+    date_borrowed DATE NOT NULL,
+    due_date DATE NOT NULL,
+    PRIMARY KEY (id)
+);
  
  
