72408b3b5e66ced59b169b9fc0dd26bef17fecbb
Dan Smith <daniel.smith8795@gmail.com>
2019-09-24 21:40:12 -0500

    fixed API routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,21 +2,21 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
+  app.get("/api/books", function(req, res) {
     db.books.findAll({}).then(function(dbBooks) {
       res.json(dbBooks);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  app.post("/api/books", function(req, res) {
     db.books.create(req.body).then(function(dbBooks) {
       res.json(dbBooks);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  app.delete("/api/books/:id", function(req, res) {
     db.books.destroy({ where: { id: req.params.id } }).then(function(dbBooks) {
       res.json(dbBooks);
     });
